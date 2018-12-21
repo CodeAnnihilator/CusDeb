a3aea0d3f91263b7f39b9e40dc2cf280ba9bb161
CodeAnnihilator <frostberryart@gmail.com>
2018-12-21 08:43:16 +0300

    added Dashboard card

--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 
 import styles from './dashboard.scss'
 
+import DebianSVG from 'assets/images/distributive/debian.svg'
+// import DevuanSVG from 'assets/images/distributive/devuan.svg'
+// import UbuntuSVG from 'assets/images/distributive/ubuntu.svg'
+
+import textSamples from './textSamples'
+
 export default class Dashboard extends Component<{}> {
   render() {
     return (
@@ -11,7 +17,18 @@ export default class Dashboard extends Component<{}> {
         </div>
         <div className={styles.container}>
           <div className={styles.images}>
-            <div>IMAGE</div>
+            <div className={styles.image}>
+              <div className={styles.image_header}>
+                <img className={styles.image_header_img} src={DebianSVG} />
+                <div className={styles.image_header_text}>
+                  <div className={styles.image_header_text_title}>DEVUAN 1 "JESSIE" (32-BIT)</div>
+                  <div className={styles.image_header_text_date}>Created at: 21.09.2018</div>
+                </div>
+              </div>
+              <div className={styles.image_note}>{ textSamples[0] }</div>
+              <div className={styles.controls}>
+              </div>
+            </div>
           </div>
           <div className={styles.descriptionBar}>
             BAR
---
+++ src/modules/Dashboard/textSamples.tsx
@@ -0,0 +1,23 @@
+const textSamples = [
+  `
+    consequat massa quis enim. Donec pede justo, fringilla vel, 
+    aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut
+  `,
+  `
+    rcu. In enim justo, rhoncus ut, 
+    imperdiet a, venenatis vitae, justo. Nullam dictum felis eu 
+    pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
+    elementum semper nisi. Aenean vulputate eleifend tellus. Aenean 
+    leo ligula, porttitor eu, consequat vitae, elei, rcu. In enim justo, rhoncus ut, 
+    imperdiet a, venenatis vitae, justo. Nullam dictum felis eu 
+    pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
+    elementum semper nisi. Aenean vulputate eleifend tellus. Aenean 
+    leo ligula, porttitor eu, consequat vitae, elei, rcu. In enim justo, rhoncus ut, 
+    imperdiet a, venenatis vitae, justo. Nullam dictum felis eu 
+    pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
+    elementum semper nisi. Aenean vulputate eleifend tellus. Aenean 
+    leo ligula, porttitor eu, consequat vitae, elei
+  `
+]
+
+export default textSamples
