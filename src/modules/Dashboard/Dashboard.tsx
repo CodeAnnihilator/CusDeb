9ae8d0553444012e41a87a8b407b037a4bc2fe9b
CodeAnnihilator <frostberryart@gmail.com>
2018-12-24 13:52:08 +0300

    added masonry field for placing images on a dashboard

--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+import cn from 'classnames'
+import Masonry from 'react-masonry-css'
 
 import styles from './dashboard.scss'
 
 import DebianSVG from 'assets/images/distributive/debian.svg'
-// import DevuanSVG from 'assets/images/distributive/devuan.svg'
-// import UbuntuSVG from 'assets/images/distributive/ubuntu.svg'
+import UbuntuSVG from 'assets/images/distributive/ubuntu.svg'
+import DevuanSVG from 'assets/images/distributive/devuan.svg'
+
+const distributives = [DebianSVG, UbuntuSVG, DevuanSVG]
+
+import ListSVG from 'assets/images/list.svg'
+import DotMatrix from 'assets/images/dot-matrix.svg'
+import DownloadSVG from 'assets/images/download.svg'
+import EditSVG from 'assets/images/edit.svg'
+import WritingSVG from 'assets/images/writing.svg'
+import TerminalSVG from 'assets/images/terminal.svg'
+import DeleteSVG from 'assets/images/delete.svg'
 
 import textSamples from './textSamples'
+
+const breakpointColumnsObj = {
+  default: 3,
+  1400: 2,
+  1150: 1
+}
 
 export default class Dashboard extends Component<{}> {
   render() {
@@ -15,20 +33,49 @@ export default class Dashboard extends Component<{}> {
         <div className={styles.byttonContainer}>
           <div className={styles.createBuildButton}>CREATE NEW BUILD</div>
         </div>
+        <div className={styles.viewTabs}>
+          <div className={cn(styles.viewTab, styles.viewTab_active)}>
+            <img className={styles.viewTab_img} src={ListSVG} />
+          </div>
+          <div className={styles.viewTab}>
+            <img className={styles.viewTab_img} src={DotMatrix} />
+          </div>
+        </div>
         <div className={styles.container}>
           <div className={styles.images}>
-            <div className={styles.image}>
-              <div className={styles.image_header}>
-                <img className={styles.image_header_img} src={DebianSVG} />
-                <div className={styles.image_header_text}>
-                  <div className={styles.image_header_text_title}>DEVUAN 1 "JESSIE" (32-BIT)</div>
-                  <div className={styles.image_header_text_date}>Created at: 21.09.2018</div>
-                </div>
-              </div>
-              <div className={styles.image_note}>{ textSamples[0] }</div>
-              <div className={styles.controls}>
-              </div>
-            </div>
+          <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className={styles.myMasonryGrid}
+            columnClassName={styles.myMasonryGridColumn}
+          >
+            {
+              Array.from({ length: 10 }, () => '').map((_, index) => {
+                return (
+                  <div key={index} className={styles.image}>
+                    <div className={styles.image_header}>
+                      <img className={styles.image_header_img} src={distributives[Math.round(Math.random() * 3)]} />
+                      <div className={styles.image_header_text}>
+                        <div className={styles.image_header_text_title}>DEVUAN 1 "JESSIE" (32-BIT)</div>
+                        <div className={styles.image_header_text_date}>Created at: 21.09.2018</div>
+                      </div>
+                    </div>
+                    <div className={styles.image_note}>{ textSamples[Math.round(Math.random() * 2)] }</div>
+                    <div className={styles.controls}>
+                      <div className={styles.controls_button}>
+                        <img className={styles.controls_button_img} src={DownloadSVG} />
+                        <span className={styles.controls_button_text}>Download</span>
+                      </div>
+                      <div className={styles.controls_devider} />
+                      <img className={styles.controls_img} src={EditSVG} />
+                      <img className={styles.controls_img} src={WritingSVG} />
+                      <img className={styles.controls_img} src={TerminalSVG} />
+                      <img className={styles.controls_img} src={DeleteSVG} />
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </Masonry>
           </div>
           <div className={styles.descriptionBar}>
             BAR
--- src/modules/Dashboard/textSamples.tsx
+++ src/modules/Dashboard/textSamples.tsx
@@ -12,12 +12,16 @@ const textSamples = [
     imperdiet a, venenatis vitae, justo. Nullam dictum felis eu 
     pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
     elementum semper nisi. Aenean vulputate eleifend tellus. Aenean 
-    leo ligula, porttitor eu, consequat vitae, elei, rcu. In enim justo, rhoncus ut, 
+    leo ligula, porttitor eu, consequat vitae, elei, rcu. In enim 
+  `,
+  ``,
+  `
+    rcu. In enim justo, rhoncus ut, 
     imperdiet a, venenatis vitae, justo. Nullam dictum felis eu 
     pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus 
     elementum semper nisi. Aenean vulputate eleifend tellus. Aenean 
-    leo ligula, porttitor eu, consequat vitae, elei
-  `
+    leo ligula, porttitor eu, consequat vitae, elei, rcu. In enim justo
+  `,
 ]
 
 export default textSamples
