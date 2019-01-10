0514cb5b5b7dac0236d3f81ec2e5ee617d959015
Alexey Gerasimov <gerasimov.alexey1994@gmail.com>
2019-01-10 19:00:10 +0300

    feature-CD-1_Added_error_state_at_ImageCard

--- src/modules/Dashboard/components/ImagesCardView/ImageCard/ImageCard.tsx
+++ src/modules/Dashboard/components/ImagesCardView/ImageCard/ImageCard.tsx
@@ -14,28 +14,61 @@ interface IProps {
 }
 
 export default class ImageCard extends PureComponent<IProps> {
+  onSelect = () => {
+    const {image, onSelect} = this.props;
+
+    onSelect(image.get('name'))
+  }
+
+  static defaultProps = {
+    image: {
+      thumb: ''
+    }
+  }
+
+  styleGetter = (tail: string) => styles[tail]
+
   render() {
-    const {
-      image,
-      onSelect,
-      isActive
-    } = this.props
+    const { image, isActive } = this.props;
+    const { styleGetter } = this;
+ 
     const notes = image.get('notes')
     const distro = image.getIn(['distro', 'full_name'])
     const startedAt = image.get('started_at')
     const isEmulate = image.get('emulate')
-    const name = image.get('name')
     const thumb = image.get('thumb')
+    const hasError = image.get('hasError')
+
     return (
       <div ref='test'className={cn(styles.wrapper, {[styles.active]: isActive})}>
-        <div className={styles.header} onClick={() => onSelect(name)}>
-          <img className={styles.header_img} src={thumb || ''} />
+        <div className={styles.header} onClick={this.onSelect}>
+          <img className={styles.header_img} src={ thumb } />
           <div className={cn(styles.header_text, {[styles.header_text__active]: isActive})}>
             <div className={styles.header_text_title}>{ distro }</div>
             <div className={styles.header_text_date}>Started at: { moment(startedAt).format('L') }</div>
           </div>
         </div>
-        { notes && <div className={styles.note} onClick={() => onSelect(name)}>{ notes }</div> }
+        { notes &&
+          <div
+            className={cn(styles.note, {[styleGetter('note--error')]: hasError})}
+            onClick={this.onSelect}
+          >
+            {!hasError ? notes : (
+              <div className={styleGetter('error-box')}>
+                <div className={styleGetter('error-box_header')}>
+                  Oops, something went wrong
+                </div>
+                <div>
+                  if the problem persists, please
+                  {' '}
+                  <a href="/dashboard">
+                    report an issue
+                  </a>
+                </div>
+              </div>
+            )}
+          </div>
+        }
         <Controls isEmulate={isEmulate} />
       </div>
     )
