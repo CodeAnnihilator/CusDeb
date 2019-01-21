dab4c66d85e03d8995b950cd062d9860747f7197
CodeAnnihilator <frostberryart@gmail.com>
2019-01-21 18:06:54 +0300

    added tslint precommit hook

--- src/assets/images/HardBookIcon.tsx
+++ src/assets/images/HardBookIcon.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 interface IProps {
-  fill?: string;
-  className?: string;
-  style?: object;
+	fill?: string;
+	className?: string;
+	style?: object;
 }
 
-const HardBookIcon: React.SFC<IProps> = ({ fill, className, style }) => (
+const HardBookIcon: React.SFC<IProps> = ({fill, className, style}) => (
 	<svg
 		fill={fill}
 		className={className}
 		style={style}
 		width='612px'
 		height='612px'
-		viewBox="0 0 612 612"
+		viewBox='0 0 612 612'
 	>
 		<g>
-			<path d="M464,64v416H80c-17.672,0-32-14.312-32-32s14.328-32,32-32h352V0H80C44.656,0,16,28.656,16,64v384
-				c0,35.344,28.656,64,64,64h416V64H464z"/>
+			<path
+				d='M464,64v416H80c-17.672,0-32-14.312-32-32s14.328-32,32-32h352V0H80C44.656,0,16,28.656,16,64v384
+				c0,35.344,28.656,64,64,64h416V64H464z'
+			/>
 		</g>
 	</svg>
-)
+);
 
-export default HardBookIcon
+export default HardBookIcon;
